4fc63f71697103588bdcf6699d3e9aa288f029f0
Oleg Nesterov <oleg9988@rambler.ru>
2022-10-14 17:15:25 +0300

    Добавление лямбда выражения которое удалят из списка тех кто младше 18 лет

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 import java.util.TreeSet;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
+        Predicate<Person> truDelete = a -> a.getAge() < 18;
         TreeSet<Person> persons = new TreeSet<>(new PersonComparator(1) {});
         persons.add(new Person("Олег", "Нестеров-Мусорский", 32));
+        persons.add(new Person("Павел", "Бухаров", 13));
         persons.add(new Person("Ярослав", "Борисов Моисеев", 41));
         persons.add(new Person("Руслан", "Сосланов", 31));
+        persons.add(new Person("Алексей", "Суриков", 9));
+        persons.removeIf(truDelete);
         System.out.println(persons);
     }
 }
